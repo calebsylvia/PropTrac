f1dadcdb6605cc485c68602f170329bca499c13c
Caleb Sylvia <caleb_sylvia@yahoo.com>
2024-04-05 10:08:32 -0700

    fix deployment issue

--- prop-trac/src/app/page.tsx
+++ prop-trac/src/app/page.tsx
@@ -1,10 +1,22 @@
 import Image from "next/image";
-import Login from "./pages/Login/page";
+import LoginComponent from "./Components/LoginComponent";
 
 export default function Home() {
   return (
     <>
-      <Login/>
+      <div className="background w-screen h-screen flex">
+        <div className="w-1/3 text-center pl-20 pt-20">
+          <h1 className="text-9xl text-[#5A5A5A]">
+            PROP
+            <br />
+            TRAC
+          </h1>
+          <p className="">Property management at your fingertips</p>
+        </div>
+        <div className="w-[400px] m-auto">
+          <LoginComponent/>
+        </div>
+      </div>
     </>
   );
 }
