331a9784f0d433462467f90eab43ae5cdc77d37e
Caleb Sylvia <caleb_sylvia@yahoo.com>
2024-05-15 16:11:57 -0700

    EOD push

--- prop-trac/src/app/Maintenance/page.tsx
+++ prop-trac/src/app/Maintenance/page.tsx
@@ -4,6 +4,8 @@ import SideNav from '../Components/SideNav'
 import TopNav from '../Components/TopNav'
 
 const Maintenance = () => {
+
+
   return (
     <>
     <div className='hidden lg:block'>
@@ -13,7 +15,24 @@ const Maintenance = () => {
       <TopNav/>
     </div>
          <div  className='bg-[#FEFFF6] w-full h-full'>
-            
+            <div className='lg:ml-52 pt-20 w-3/4'>
+                <div className='bg-white border-2 border-black rounded-xl grid grid-cols-3'>
+                    <div>
+
+                    </div>
+                    <div>
+
+                    </div>
+                    <div>
+
+                    </div>
+                </div>
+                <div className='bg-[#EEE2D1] rounded-xl py-5 px-4'>
+                    <div className='bg-white rounded-xl w-72 h-36'>
+                      
+                    </div>
+                </div>
+            </div>
         </div>
     </>
   )
