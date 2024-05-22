171ab20d9a26af5ae3b3fc0b672a3782d52bcd1b
Caleb Sylvia <caleb_sylvia@yahoo.com>
2024-05-22 11:36:33 -0700

    fix map?

--- prop-trac/src/app/Components/MapComponent.tsx
+++ prop-trac/src/app/Components/MapComponent.tsx
@@ -1,31 +1,26 @@
 "use client";
 import React from "react";
 import { IPosition } from "@/Interfaces/Interfaces";
-import ReactMapboxGl, { Marker } from "react-mapbox-gl";
 import downRed from "@/app/Assets/downRed.png";
+import { Map, Marker } from "react-map-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 import Image from "next/image";
 
 const MapComponent = ({ lat, lng }: IPosition) => {
-  const Map = ReactMapboxGl({
-    accessToken: process.env.NEXT_PUBLIC_MAP_KEY!,
-    scrollZoom: true,
-    logoPosition: "bottom-right",
-    minZoom: 15,
-  });
+
 
   return (
     <>
       <Map
-        style="mapbox://styles/mapbox/streets-v12"
-        center={[lat, lng]}
-        containerStyle={{
-          height: "100%",
+        mapboxAccessToken={process.env.NEXT_PUBLIC_MAP_KEY!}
+        mapStyle="mapbox://styles/mapbox/streets-v12"
+        initialViewState={{
+          longitude: lng,
+          latitude: lat,
+          zoom: 14
         }}
+        style={{height: '100%'}}
       >
-        <Marker coordinates={[lat, lng]} anchor="bottom">
-          <Image src={downRed} alt="Marker Icon"/>
-        </Marker>
       </Map>
     </>
   );
